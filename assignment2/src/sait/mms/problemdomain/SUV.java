f70efdf5cd3dba85b1017cb54802eac12158bfbe
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2025-10-25 16:33:52 -0600

    Implement SUV constructor and toString method
    Added constructor and toString method to SUV class.

--- assignment2/src/sait/mms/problemdomain/SUV.java
+++ assignment2/src/sait/mms/problemdomain/SUV.java
@@ -1,5 +1,35 @@
 package sait.mms.problemdomain;
-
+ 
 public class SUV extends Vehicle{
-
+	
+	
+	
+	
+	//constructor
+	
+	//CarID;VehicleType;Subtype;Speed;Fuel;Seats;Year;Drivetrain;Price;Quantity
+	public SUV(String CarID, String VehicleType, String SubType, int Speed, double Fuel, int Seats, int Year, String Drivetrain, double Price, int Quantity) {
+		setCarID(CarID);
+		setVehicleType(VehicleType);
+		setSubType(SubType);
+		setSpeed(Speed);
+		setFuel(Fuel);
+		setSeats(Seats);
+		setYear(Year);
+		setDrivetrain(Drivetrain);
+		setPrice(Price);
+		setQuantity(Quantity);
+		
+		
+		
+	}
+	//toString
+	@Override
+	public String toString() {
+		return "Car ID: " + getCarID() + "\nVehicle type: " + getVehicleType() + "\nSub type: " + getSubType() + "\nSpeed: " + getSpeed()+ "km/h\nFuel: " + getFuel() + 
+				"L\nNumber of Seats: " + getSeats() + "\nYear: " + getYear() + "\nDriveTrain: " + getDrivetrain() + "\nPrice: $" + getPrice() + 
+				"\nAvailable: " + getQuantity() + "\nDriveTrain: " + getDrivetrain();
+	}
+	
+	
 }
