fb6efcbaad6d1dc590ad817ce2d86d3ee55034c2
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2025-10-25 16:33:15 -0600

    Update Sedan.java

--- assignment2/src/sait/mms/problemdomain/Sedan.java
+++ assignment2/src/sait/mms/problemdomain/Sedan.java
@@ -1,5 +1,60 @@
 package sait.mms.problemdomain;
-
-public class Sedan {
-
+ 
+public class Sedan extends Vehicle{
+	
+	private String TrunkSize;
+	
+	//getters and setters
+	public String getTrunkSize() {
+		return TrunkSize;
+	}
+ 
+	public void setTrunkSize(String trunkSize) {
+		TrunkSize = trunkSize;
+	}
+	//constructor
+	
+	//CarID;VehicleType;Subtype;Speed;Fuel;Seats;Year;Drivetrain;Price;Quantity;TrunkSize
+	public Sedan(String CarID, String VehicleType, String SubType, int Speed, double Fuel, int Seats, int Year, String Drivetrain, double Price, int Quantity, String TrunkSize) {
+		setCarID(CarID);
+		setVehicleType(VehicleType);
+		setSubType(SubType);
+		setSpeed(Speed);
+		setFuel(Fuel);
+		setSeats(Seats);
+		setYear(Year);
+		setDrivetrain(Drivetrain);
+		setPrice(Price);
+		setQuantity(Quantity);
+		this.TrunkSize = TrunkSize;
+		
+		
+	}
+	//converts trunk size of L, M or S to a full description
+	public String trunkSizeDetailed() {
+		String trunkSizeMessage = null; 
+		if (TrunkSize == "L") {
+			trunkSizeMessage = "Large/spacious trunk";
+			
+		}
+		if (TrunkSize == "S") {
+			trunkSizeMessage = "Small";
+			
+		}
+		if (TrunkSize == "M") {
+			trunkSizeMessage = "Moderate";
+			
+		}
+		return trunkSizeMessage;
+	}
+	//toString
+	@Override
+	public String toString() {
+		return "Car ID: " + getCarID() + "\nVehicle type: " + getVehicleType() + "\nSub type: " + getSubType() + "\nSpeed: " + getSpeed()+ "km/h\nFuel: " + getFuel() + 
+				"L\nNumber of Seats: " + getSeats() + "\nYear: " + getYear() + "\nDriveTrain: " + getDrivetrain() + "\nPrice: $" + getPrice() + 
+				"\nAvailable: " + getQuantity() + "\nTrunk Size: " + trunkSizeDetailed();
+		 
+	}
+	
+	
 }
