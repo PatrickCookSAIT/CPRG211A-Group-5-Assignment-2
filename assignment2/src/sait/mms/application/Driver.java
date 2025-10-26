8ada5b15db25650a3b97477f1eb651b4f0a23ae9
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2025-10-26 16:37:50 -0600

    Update Driver.java

--- assignment2/src/sait/mms/application/Driver.java
+++ assignment2/src/sait/mms/application/Driver.java
@@ -1,6 +1,10 @@
+//Patrick Cook & Jace Borden 10/26/2025
+
 package sait.mms.application;
 
 import sait.mms.manager.DriveMastersManager;
+
+//loads DriveMastersManager to run program
 
 public class Driver {
 	public static void main(String[] args) 
@@ -10,3 +14,4 @@ public class Driver {
 
 	}
 }
+
